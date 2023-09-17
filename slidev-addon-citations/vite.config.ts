7e95d4b7405ca9d8f9858bd46a3803889a5d4986
Alexandre Eudes <alexandre.eudes@gmail.com>
2023-09-17 23:30:34 +0200

    worked globally and locally

--- slidev-addon-citations/vite.config.ts
+++ slidev-addon-citations/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     optimizeDeps: {
-     include: [ '@citation-js/core > @citation-js/date','@citation-js/core > @citation-js/name','@citation-js/plugin-csl > citeproc','moo'],//,'@citation-js/name','@citation-js/date'],// 'fetch-ponyfill','moo','sync-fetch' ], //, 'sync-fetch','fetch-ponyfill','moo'],
+     include: [ '@citation-js/core > @citation-js/date','@citation-js/core > @citation-js/name','@citation-js/plugin-csl > citeproc','moo','sync-fetch','fetch-ponyfill','slidev-addon-citations > @citation-js/name','slidev-addon-citations > @citation-js/date','slidev-addon-citations > citeproc','slidev-addon-citations > fetch-ponyfill','slidev-addon-citations > sync-fetch','slidev-addon-citations > moo'],//,'@citation-js/name','@citation-js/date'],// 'fetch-ponyfill','moo','sync-fetch' ],
      //include: [ '@citation-js/name','citeproc','@citation-js/date', 'fetch-ponyfill','moo','sync-fetch' ], //, 'sync-fetch','fetch-ponyfill','moo'],
     // exclude: ['@citation-js/core']
   },
